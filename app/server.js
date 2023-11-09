082442f9601f20d3b741ca9f1d31f2e67d505d60
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-11-09 17:15:06 +0100

    Update

--- app/server.js
+++ app/server.js
@@ -23,7 +23,7 @@ mongoose
 app.use(FrontRouter);
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
